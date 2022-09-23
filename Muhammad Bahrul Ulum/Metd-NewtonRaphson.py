65128b529b00b27965525224dcab19873dc1f37d
Pascal_00 <88598619+ulm12@users.noreply.github.com>
2022-09-23 09:41:21 +0700

    Update Metd-NewtonRaphson.py

--- Muhammad Bahrul Ulum/Metd-NewtonRaphson.py
+++ Muhammad Bahrul Ulum/Metd-NewtonRaphson.py
@@ -4,10 +4,7 @@ import numpy as np
 from math import sqrt
 
 
-import numpy as np #panggil library
-
-
-def my_newton(f, df, x0, e):
+def newton_raphson(f, df, x0, e):
 
     print("Nilai xn: ", x0)
 
@@ -15,7 +12,7 @@ def my_newton(f, df, x0, e):
         return x0
     else:
         xn = x0 - f(x0)/df(x0)
-        return my_newton(f, df, xn, e)
+        return newton_raphson(f, df, xn, e)
 
 
 fx = lambda x: ((x**2)-(2*x)-8)
@@ -24,7 +21,7 @@ f_prime = lambda x: 2*x - 2
 
 n = float(input())
 
-estimate = my_newton(fx, f_prime, n, 1e-3)
+estimate = newton_raphson(fx, f_prime, n, 1e-3)
 
 print("estimate =", estimate)
 
